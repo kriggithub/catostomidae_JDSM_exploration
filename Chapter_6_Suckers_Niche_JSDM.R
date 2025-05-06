710c9f8164e2707d8cc394ceeb3f0ccb24955d80
Kurt Riggin <krigscholar@gmail.com>
2025-05-06 00:39:45 -0600

    Reorganize data cleaning folder and adjust species names to match for easier anaylsis.

--- Chapter_6_Suckers_Niche_JSDM.R
+++ Chapter_6_Suckers_Niche_JSDM.R
@@ -1,8 +1,16 @@
-#### K. Riggin April 22, 2025
-#### Single Species Distribution Modeling
-#### Chosen species: White Sucker (present in 199 sites with max count of 169)
+#### K. Riggin May 5, 2025
+#### Joint Species Distribution Modeling Traits and Phylogeny
 
 library(ape)
+
+
+data <- read.csv("suckerspresencedata.csv")
+data$elevation <- data$elevation/100
+
+# separate model construction and validation sites
+data_C <- data %>% filter(val_cond == "C")
+data_V <- data %>% filter(val_cond == "V")
+
 
 
 tree <- read.nexus("mito_tree.nex")
@@ -10,6 +18,11 @@ plot(tree)
 
 # construct phylogenetic correlation matrix
 C <- vcv(tree, model = "Brownian", corr = T)
+
+
+
+#influence of traits on suckers
+
 
 
 
--- data_cleaning/Data_Prep_Code.R
+++ data_cleaning/Data_Prep_Code.R
@@ -73,3 +73,122 @@ table(reference_table$ref_cond, reference_table$val_cond, reference_table$AG_ECO
 
 
 
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+#### K. Riggin April 22, 2025
+#### This code produces subsetted data for the Catostomidae family, trait data, and a phylogenetic tree to be used for analysis
+
+####Step 6: Subset Data sets to only include Catostomids and Same SITE_ID
+library(rfishbase)
+
+# Filter taxa down to the desired species
+suckers <- load_taxa() %>%  filter(Family == "Catostomidae")
+
+# Create new dataframe with scientific species names from data
+colnames(fish_table)
+scinames <- common_to_sci(colnames(fish_table))
+
+
+# Filter scientific names to only include Catostomids 
+scinames <- scinames %>%
+  filter(Species %in% suckers$Species)
+
+
+# Change all common and scientific names into lowercase with words separated by periods in all data to match
+scinames$Species <- scinames$Species %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+scinames$ComName <- scinames$ComName %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+names(fish_table)[2:363] <- names(fish_table)[2:363] %>% 
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+
+
+# Create subsetted table
+fish_table_subset <- fish_table %>%
+  select("SITE_ID", any_of(scinames$ComName))
+
+
+
+# Match Environmetal and Reference Table Sites with Fish Table
+environmental_table_subset <- environmental_table %>%
+  filter(SITE_ID %in% fish_table_subset$SITE_ID)
+reference_table_subset <- reference_table %>%
+  filter(SITE_ID %in% fish_table_subset$SITE_ID)
+
+
+# Join together Fish, Environmental, and Reference Tables, and select only reference sites
+data <- environmental_table_subset %>%
+  left_join(reference_table_subset %>% select(SITE_ID, AG_ECO9, ref_cond, val_cond), by = "SITE_ID") %>% 
+  left_join(fish_table_subset, by = "SITE_ID") %>% filter(ref_cond == "REF")
+
+
+# Adjust elevation to be in meters instead of centimeters
+data$elevation <- data$elevation/100
+
+# Create seperate dataframe for presence/absence data
+data_pres <- data %>% mutate(across(golden.redhorse:torrent.sucker, ~ ifelse(. > 0, 1, 0)))
+
+# Pull out sucker common and scientific names from data for other dataframes to reference
+data_cat_comnames <- names(data)[17:44] # common names to use
+
+  # Match common names exactly
+cat_scicomnames <- common_to_sci(data_cat_comnames)
+cat_scicomnames$Species <- cat_scicomnames$Species %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+cat_scicomnames$ComName <- cat_scicomnames$ComName %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+cat_scicomnames <- cat_scicomnames %>%
+  distinct(ComName, .keep_all = TRUE)
+cat_scicomnames <- cat_scicomnames[cat_scicomnames$ComName %in% data_cat_comnames, ]
+
+data_cat_scinames <- as.character(cat_scicomnames$Species)
+
+
+# Export data for analysis (in main directory)
+# write.csv(data, file = "suckersabundancedata.csv")
+# write.csv(data_pres, file = "suckerspresencedata.csv")
+
+
+
+
+
+
+####Step 7: Create Trait Data to be used from (taken from https://www.sciencebase.gov/catalog/item/5a7c6e8ce4b00f54eb2318c0)
+trait_dat <- read.csv("FishTraits_14.3.csv")
+
+
+# Match format of common and other names in trait data to community data
+trait_dat$COMMONNAME <- trait_dat$COMMONNAME %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+trait_dat$OTHERNAMES <- trait_dat$OTHERNAMES %>%
+  str_to_lower() %>%
+  str_replace_all(" ", ".")
+
+
+
+# pick traits of interest (length, temperature preference, substrate preference, current) and filter by data_cat_comnames
+trait_dat <- trait_dat %>% select(COMMONNAME, OTHERNAMES, MAXTL, MINTEMP, MAXTEMP, MUCK:LWD, SLOWCURR:FASTCURR) %>% 
+  filter(COMMONNAME %in% data_cat_comnames | OTHERNAMES %in% data_cat_comnames)
+
+# Export trait data for main directory
+# write.csv(trait_dat, file = "catostomidae_traits.csv")
+
+
+
+
+####Step 7: Create phylogenetic tree to use for anaylsis
+
+
+
+
+
+
+
